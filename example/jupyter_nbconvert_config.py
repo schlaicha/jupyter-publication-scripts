a827bd6f3bdca63a8cb77be73d8b1b2938c2894d
Julius Schulz <julius.schulz@fu-berlin.de>
2015-12-02 20:02:17 +0100

    added example notebook demonstrating all added features

--- example/jupyter_nbconvert_config.py
+++ example/jupyter_nbconvert_config.py
@@ -1,3 +1,4 @@
 c = get_config()
 c.Exporter.preprocessors = [ 'bibpreprocessor.BibTexPreprocessor', 'pymdpreprocessor.PyMarkdownPreprocessor', 'mdpreprocess.MarkdownPreprocessor' ]
+c.TemplateExporter.template_path = ['../templates','.']
 c.Exporter.template_file = 'revtex_nocode.tplx'
