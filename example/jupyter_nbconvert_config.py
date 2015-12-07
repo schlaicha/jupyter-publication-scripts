4adec7f4ac1000f9c4b968571f38850b156a9970
Julius Schulz <julius.schulz@fu-berlin.de>
2015-12-07 15:50:44 +0100

    modified jupyter config to follow changed filenames

--- example/jupyter_nbconvert_config.py
+++ example/jupyter_nbconvert_config.py
@@ -1,4 +1,4 @@
 c = get_config()
-c.Exporter.preprocessors = [ 'pre_cite2c.BibTexPreprocessor', 'pymdpreprocessor.PyMarkdownPreprocessor', 'mdpreprocess.MarkdownPreprocessor' ]
+c.Exporter.preprocessors = [ 'pre_cite2c.BibTexPreprocessor', 'pymdpreprocessor.PyMarkdownPreprocessor', 'pre_markdown.MarkdownPreprocessor' ]
 c.TemplateExporter.template_path = ['../templates','.']
 c.Exporter.template_file = 'revtex_nocode.tplx'
