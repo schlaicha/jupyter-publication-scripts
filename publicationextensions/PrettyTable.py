534a3eaa18b7c1062b8053c597de02eca0079c92
Alexander Schlaich <aschlaich@physik.fu-berlin.de>
2016-01-06 12:31:28 +0100

    Teach PrettyTable the table* environment
    According to the RevTex documentation this is the proposed way to span
    figures/tables across columns.
    
    However, the current implementation in latex_nocode.tplx does not allow
    for easy modification. Therefore in the meanwhile we keep this inside
    a development branch.

--- publicationextensions/PrettyTable.py
+++ publicationextensions/PrettyTable.py
@@ -8,7 +8,8 @@ class PrettyTable(list):
         If the list is one dimensional it is converted to a single-column list, extra_header may
         then be either a list containing a single element or a string.
         """
-    def __init__(self, initlist=[], extra_header=None, print_latex_longtable=True, formatstring=""):
+    def __init__(self, initlist=[], extra_header=None, print_latex_longtable=True, 
+            span_page=False, formatstring=""):
         """
         Public constructor
 
@@ -20,10 +21,13 @@ class PrettyTable(list):
             list of captions for each column of initlist, can also be a string for one dimensional lists
         print_latex_longtable : bool
             if True create longtable in latex representation, otherwise output a simple tabular environment
+        span_page : bool
+            if True make table span the page using tge table* environment
         formatstring : str
             custom format string for number conversion
         """
         self.print_latex_longtable = print_latex_longtable
+        self.span_page = span_page
         self.formatstring = formatstring
         if not isinstance(initlist[0], collections.Iterable):
             initlist = map(lambda x: [x], initlist)
@@ -37,15 +41,28 @@ class PrettyTable(list):
         super(PrettyTable, self).__init__(initlist)
 
     def latex_table_tabular(self):
-        latex = ["\\begin{tabular}"]
+        latex = []
+        if self.span_page:
+            latex.append("\\begin{table*}")
+        else:
+            latex.append("\\begin{table}")
+        latex.append("\\begin{tabular}")
         latex.append("{"+"|".join((["l"]*len(self[0])))+"}\n")
         for row in self:
             latex.append(" & ".join(map(format, row)))
             latex.append("\\\\ \n")
         latex.append("\\end{tabular}")
+        if self.span_page:
+            latex.append("\\end{table*}")
+        else:
+            latex.append("\\end{table}")
         return ''.join(latex)
     def latex_longtable(self):
-        latex = ["\\begin{longtable}[c]{@{}"]
+        latex = []
+        if self.span_page:
+            latex.append("\\begin{longtable*}[c]{@{}")
+        else:
+            latex.append("\\begin{longtable}[c]{@{}")
         latex.append("".join((["l"]*len(self[0]))))
         latex.append("@{}}\n")
         latex.append("\\toprule\\addlinespace\n")
@@ -56,7 +73,10 @@ class PrettyTable(list):
             if first:
                 latex.append("\\midrule\\endhead\n")
                 first = False
-        latex.append("\\bottomrule \n \\end{longtable}")
+        if self.span_page:
+            latex.append("\\bottomrule \n \\end{longtable*}")
+        else:
+            latex.append("\\bottomrule \n \\end{longtable}")
         return ''.join(latex)
 
     def _repr_html_(self):
