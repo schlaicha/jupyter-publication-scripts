619de90413f4d2582a3dfb446ffa8345131ac61f
Julius Schulz <julius.schulz@fu-berlin.de>
2016-01-11 16:22:32 +0100

    adressed issue #8 and added all types of tables to jinja template

--- example/jupyter_nbconvert_config.py
+++ example/jupyter_nbconvert_config.py
@@ -3,3 +3,9 @@ c.Exporter.preprocessors = [ 'pre_cite2c.BibTexPreprocessor', 'pymdpreprocessor.
 c.TemplateExporter.template_path = ['../templates','.']
 c.Exporter.template_file = 'revtex_nocode.tplx'
 #c.Exporter.template_file = 'article_nocode.tplx'
+
+import re
+def re_replace(vars):
+    #print re.sub(vars[1], vars[2], vars[0])
+    return re.sub(vars[1], vars[2], vars[0])
+c.TemplateExporter.filters = { "re_replace" : re_replace}
